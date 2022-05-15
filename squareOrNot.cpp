adaf85f6bbfaf760ab0257cd2ab6b8ea49c8d4c1
Ravichandran <104997943+RvcReaTioN@users.noreply.github.com>
2022-05-15 22:29:39 +0530

    Add files via upload

---
+++ squareOrNot.cpp
@@ -0,0 +1,25 @@
+//entered values of length and breadth of a rectangle from user and check if it is square or not
+#include<iostream>
+using namespace std;
+int main()
+{
+    float length,breadth,square,notSquare;
+    
+    cout<<endl<<"enter the value of length = ";
+    cin>>length;
+    cout<<endl<<"enter the value of breadth = ";
+    cin>>breadth;
+    
+    if (length == breadth)
+    {
+        cout<<endl<<"data enterd "<<length<<" and "<<breadth<<" are SQUARE";
+        
+    }
+    if (length != breadth)
+    {
+       cout<<endl<<"data enterd "<<length<<" and "<<breadth<<" are NOT A SQUARE ";
+
+    }
+
+    return 0;
+};
