f214e192ff39691571c109d3863a9ac7e0642d59
Ravichandran <104997943+RvcReaTioN@users.noreply.github.com>
2022-05-17 11:33:10 +0530

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -1,12 +1,11 @@
-//  operators addtion subration multiplication division basics
+//  operators addtion subration multiplication division 
 // addition
 #include<iostream>
 #include<cmath>
 using namespace std;
 int main()
 {
-  int a,b;
-  int sum;
+  int a,b,sum;
   cout<<endl<<"enter the value of a : ";
   cin>>a;
   cout<<endl<<"enter the value of b : ";
