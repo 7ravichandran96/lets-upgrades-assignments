0ee54a1201d75e81faec82e41ada517e522e218d
Ravichandran <104997943+RvcReaTioN@users.noreply.github.com>
2022-06-01 18:27:12 +0530

    Add files via upload

---
+++ armstrongNumbers.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+  int n, i, num, digit, sum, count,j,m;
+
+  cout << "Enter range of value : ";
+  cin >> n; 
+  
+  
+  cout << "Armstrong numbers between 1 to "<< n << " = " ;
+ 
+  
+  for(i = 1; i <= n; i++)
+   {
+   
+    count = 0;
+         
+    num = i;
+
+    while(num > 0)
+     {
+      count++;
+      num /= 10;
+     }
+ 
+    sum = 0;
+    num = i;
+     
+    while(num > 0)
+    {
+      digit = num % 10;
+
+      m=1;
+      for(j=1;j<=count;j++)
+          m*=digit;
+
+      sum = sum + m;
+      num /= 10;
+    }
+
+    
+    if(sum == i) 
+    {
+      cout <<"'"<< i << "',";
+    }
+  }
+
+  return 0;
+}
